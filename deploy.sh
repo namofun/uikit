12a0f85cb6e340220ac30775053769c342552a84
小羊 <tlylz99@live.cn>
2020-09-05 23:53:20 +0800

    Fix nupkg deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 NUGET_KEY=$1
 dotnet pack -c Release -o packages
-dotnet nuget push -s https://nuget.xylab.fun/v3/index.json -k $NUGET_KEY packages/*.nupkg
+cd packages
+dotnet nuget push -s https://nuget.xylab.fun/v3/index.json -k $NUGET_KEY "*.nupkg"
