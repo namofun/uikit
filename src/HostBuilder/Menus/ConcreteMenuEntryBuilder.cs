0248c247e55c065a0eb603cae7475cd288127dc1
XiaoYang <670171961@qq.com>
2021-01-08 21:39:36 +0800

    Split ConcreteMenuEntryBase.cs

---
+++ src/HostBuilder/Menus/ConcreteMenuEntryBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Microsoft.AspNetCore.Mvc.Menus
+{
+    /// <inheritdoc cref="IMenuEntryBuilder" />
+    internal class ConcreteMenuEntryBuilder : ConcreteMenuEntryBase, IMenuEntryBuilder, IMenuEntry
+    {
+        /// <inheritdoc />
+        public override string[] ToCheck => Array.Empty<string>();
+
+        /// <summary>
+        /// Whether this entry is finalized.
+        /// </summary>
+        private bool finalized;
+
+        /// <inheritdoc />
+        public override void Contribute()
+        {
+            if (finalized) return;
+            finalized = true;
+            base.Contribute();
+        }
+
+        /// <inheritdoc />
+        public override bool Finalized => finalized;
+    }
+}
