a5e7af2f351dc182e233887d8d277c9bc2b5936e
小羊 <tlylz99@live.cn>
2021-03-30 21:27:42 +0800

    Partial works on SignController.

--- src/Extensions.IdentityModel/Controllers/SignController.cs
+++ src/Extensions.IdentityModel/Controllers/SignController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
 using SatelliteSite.IdentityModule.Models;
 using SatelliteSite.Services;
 using System;
@@ -17,19 +18,26 @@ namespace SatelliteSite.IdentityModule.Controllers
     [AuditPoint(AuditlogType.User)]
     public class SignController : ViewControllerBase
     {
-        private IUserManager UserManager { get; }
-        private ISignInManager SignInManager { get; }
-        private IConfigurationRegistry Configurations { get; }
-        private IEmailSender EmailSender { get; }
-        private IMediator Mediator { get; }
+        private readonly IUserManager _userManager;
+        private readonly ISignInManager _signInManager;
+        private readonly IConfigurationRegistry _configuration;
+        private readonly IEmailSender _emailSender;
+        private readonly IMediator _mediator;
+        private readonly IStringLocalizer<SignController> _localizer;
 
-        public SignController(ISignInManager signInManager, IConfigurationRegistry registry, IEmailSender emailSender, IMediator mediator)
+        public SignController(
+            ISignInManager signInManager,
+            IConfigurationRegistry registry,
+            IEmailSender emailSender,
+            IMediator mediator,
+            IStringLocalizer<SignController> localizer)
         {
-            SignInManager = signInManager;
-            UserManager = signInManager.UserManager;
-            Configurations = registry;
-            EmailSender = emailSender;
-            Mediator = mediator;
+            _signInManager = signInManager;
+            _userManager = signInManager.UserManager;
+            _configuration = registry;
+            _emailSender = emailSender;
+            _mediator = mediator;
+            _localizer = localizer;
         }
 
 
@@ -55,7 +63,7 @@ namespace SatelliteSite.IdentityModule.Controllers
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, set lockoutOnFailure: true
-            var result = await SignInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
+            var result = await _signInManager.PasswordSignInAsync(model.Username, model.Password, model.RememberMe, lockoutOnFailure: false);
 
             if (result.Succeeded)
             {
@@ -71,7 +79,7 @@ namespace SatelliteSite.IdentityModule.Controllers
             }
             else
             {
-                ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                ModelState.AddModelError(string.Empty, _localizer["Invalid login attempt."]);
                 return View(model);
             }
         }
@@ -89,7 +97,7 @@ namespace SatelliteSite.IdentityModule.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Logout()
         {
-            await SignInManager.SignOutAsync();
+            await _signInManager.SignOutAsync();
             return Redirect("/");
         }
 
@@ -108,7 +116,7 @@ namespace SatelliteSite.IdentityModule.Controllers
         public async Task<IActionResult> LoginWith2fa(bool rememberMe, string returnUrl = null)
         {
             // Ensure the user has gone through the username & password screen first
-            var user = await SignInManager.GetTwoFactorAuthenticationUserAsync();
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
             if (user == null) return TwoFactorFail();
 
             ViewData["ReturnUrl"] = returnUrl;
@@ -124,11 +132,11 @@ namespace SatelliteSite.IdentityModule.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
-            var user = await SignInManager.GetTwoFactorAuthenticationUserAsync();
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
             if (user == null) return TwoFactorFail();
 
             var authenticatorCode = model.TwoFactorCode.Replace(" ", string.Empty).Replace("-", string.Empty);
-            var result = await SignInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, rememberMe, model.RememberMachine);
+            var result = await _signInManager.TwoFactorAuthenticatorSignInAsync(authenticatorCode, rememberMe, model.RememberMachine);
 
             if (result.Succeeded)
             {
@@ -140,7 +148,7 @@ namespace SatelliteSite.IdentityModule.Controllers
             }
             else
             {
-                ModelState.AddModelError(string.Empty, "Invalid authenticator code.");
+                ModelState.AddModelError(string.Empty, _localizer["Invalid authenticator code."]);
                 return View();
             }
         }
@@ -152,7 +160,7 @@ namespace SatelliteSite.IdentityModule.Controllers
         public async Task<IActionResult> LoginWithRecoveryCode(string returnUrl = null)
         {
             // Ensure the user has gone through the username & password screen first
-            var user = await SignInManager.GetTwoFactorAuthenticationUserAsync();
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
             if (user == null) return TwoFactorFail();
 
             ViewData["ReturnUrl"] = returnUrl;
@@ -168,11 +176,11 @@ namespace SatelliteSite.IdentityModule.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
-            var user = await SignInManager.GetTwoFactorAuthenticationUserAsync();
+            var user = await _signInManager.GetTwoFactorAuthenticationUserAsync();
             if (user == null) return TwoFactorFail();
 
             var recoveryCode = model.RecoveryCode.Replace(" ", string.Empty);
-            var result = await SignInManager.TwoFactorRecoveryCodeSignInAsync(recoveryCode);
+            var result = await _signInManager.TwoFactorRecoveryCodeSignInAsync(recoveryCode);
 
             if (result.Succeeded)
             {
@@ -184,7 +192,7 @@ namespace SatelliteSite.IdentityModule.Controllers
             }
             else
             {
-                ModelState.AddModelError(string.Empty, "Invalid recovery code entered.");
+                ModelState.AddModelError(string.Empty, _localizer["Invalid recovery code entered."]);
                 return View();
             }
         }
@@ -198,7 +206,7 @@ namespace SatelliteSite.IdentityModule.Controllers
         {
             // Request a redirect to the external login provider.
             var redirectUrl = Url.Action(nameof(ExternalLoginCallback), "Sign", new { returnUrl });
-            var properties = SignInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider, redirectUrl);
             return Challenge(properties, provider);
         }
 
@@ -210,18 +218,18 @@ namespace SatelliteSite.IdentityModule.Controllers
         {
             if (remoteError != null)
             {
-                StatusMessage = $"Error from external provider: {remoteError}";
+                StatusMessage = _localizer["Error from external provider: {0}", remoteError];
                 return RedirectToAction(nameof(Login));
             }
 
-            var info = await SignInManager.GetExternalLoginInfoAsync();
+            var info = await _signInManager.GetExternalLoginInfoAsync();
             if (info == null)
             {
                 return RedirectToAction(nameof(Login));
             }
 
             // Sign in the user with this external login provider if the user already has a login.
-            var result = await SignInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
+            var result = await _signInManager.ExternalLoginSignInAsync(info.LoginProvider, info.ProviderKey, isPersistent: false, bypassTwoFactor: true);
             if (result.Succeeded)
             {
                 return RedirectToLocal(returnUrl);
@@ -232,7 +240,7 @@ namespace SatelliteSite.IdentityModule.Controllers
             }
             else
             {
-                if (await Configurations.GetBooleanAsync("enable_register") == false)
+                if (await _configuration.GetBooleanAsync("enable_register") == false)
                     return ExternalRegisterClosed();
 
                 // If the user does not have an account, then ask the user to create an account.
@@ -250,12 +258,12 @@ namespace SatelliteSite.IdentityModule.Controllers
         [IdentityAdvancedFeature(nameof(IdentityAdvancedOptions.ExternalLogin))]
         public async Task<IActionResult> ExternalLoginConfirmation(ExternalLoginModel model, string returnUrl = null)
         {
-            if (await Configurations.GetBooleanAsync("enable_register") == false)
+            if (await _configuration.GetBooleanAsync("enable_register") == false)
                 return ExternalRegisterClosed();
 
             var check = new RegisterNotification(model.Username);
-            await Mediator.Publish(check);
-            if (check.Failed) ModelState.AddModelError("xys::custom_rule", "The username is invalid. Please change another one.");
+            await _mediator.Publish(check);
+            if (check.Failed) ModelState.AddModelError("xys::custom_rule", _localizer["The username is invalid. Please change another one."]);
 
             if (!ModelState.IsValid)
             {
@@ -264,24 +272,24 @@ namespace SatelliteSite.IdentityModule.Controllers
             }
 
             // Get the information about the user from the external login provider
-            var info = await SignInManager.GetExternalLoginInfoAsync();
+            var info = await _signInManager.GetExternalLoginInfoAsync();
             if (info == null)
             {
                 return Message(
-                    title: "External login",
-                    message: "Error loading external login information during confirmation.",
+                    title: _localizer["External login"],
+                    message: _localizer["Error loading external login information during confirmation."],
                     type: BootstrapColor.danger);
             }
 
-            var user = UserManager.CreateEmpty(model.Username);
+            var user = _userManager.CreateEmpty(model.Username);
             user.Email = model.Email;
             user.RegisterTime = DateTimeOffset.Now;
-            var result = await UserManager.CreateAsync(user);
-            if (result.Succeeded) result = await UserManager.AddLoginAsync(user, info);
+            var result = await _userManager.CreateAsync(user);
+            if (result.Succeeded) result = await _userManager.AddLoginAsync(user, info);
 
             if (result.Succeeded)
             {
-                await SignInManager.SignInAsync(user, isPersistent: false);
+                await _signInManager.SignInAsync(user, isPersistent: false);
 
                 await HttpContext.AuditAsync(
                     "registered",
@@ -301,7 +309,7 @@ namespace SatelliteSite.IdentityModule.Controllers
         [AllowAnonymous]
         public async Task<IActionResult> Register(string returnUrl = null)
         {
-            if (await Configurations.GetBooleanAsync("enable_register") == false)
+            if (await _configuration.GetBooleanAsync("enable_register") == false)
                 return RegisterClosed();
 
             ViewData["ReturnUrl"] = returnUrl;
@@ -314,28 +322,28 @@ namespace SatelliteSite.IdentityModule.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterModel model, string returnUrl = null)
         {
-            if (await Configurations.GetBooleanAsync("enable_register") == false)
+            if (await _configuration.GetBooleanAsync("enable_register") == false)
                 return RegisterClosed();
 
             var check = new RegisterNotification(model.UserName);
-            await Mediator.Publish(check);
-            if (check.Failed) ModelState.AddModelError("xys::custom_rule", "The username is invalid. Please change another one.");
+            await _mediator.Publish(check);
+            if (check.Failed) ModelState.AddModelError("xys::custom_rule", _localizer["The username is invalid. Please change another one."]);
 
             ViewData["ReturnUrl"] = returnUrl;
             if (!ModelState.IsValid) return View(model);
 
-            var user = UserManager.CreateEmpty(model.UserName);
+            var user = _userManager.CreateEmpty(model.UserName);
             user.Email = model.Email;
             user.RegisterTime = DateTimeOffset.Now;
 
-            var result = await UserManager.CreateAsync(user, model.Password);
+            var result = await _userManager.CreateAsync(user, model.Password);
 
             if (!result.Succeeded) return ErrorView(result, model);
 
             if (user.Id == 1)
-                await UserManager.AddToRoleAsync(user, "Administrator");
+                await _userManager.AddToRoleAsync(user, "Administrator");
 
-            var code = await UserManager.GenerateEmailConfirmationTokenAsync(user);
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 
             var callbackUrl = Url.Action(
                 action: "ConfirmEmail",
@@ -343,9 +351,9 @@ namespace SatelliteSite.IdentityModule.Controllers
                 values: new { userId = $"{user.Id}", code, area = "Account" },
                 protocol: Request.Scheme);
 
-            await EmailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
+            await _emailSender.SendEmailConfirmationAsync(model.Email, callbackUrl);
 
-            await SignInManager.SignInAsync(user, isPersistent: false);
+            await _signInManager.SignInAsync(user, isPersistent: false);
 
             await HttpContext.AuditAsync(
                 "registered",
@@ -371,24 +379,24 @@ namespace SatelliteSite.IdentityModule.Controllers
         {
             if (!ModelState.IsValid) return View(model);
 
-            var user = await UserManager.FindByEmailAsync(model.Email);
+            var user = await _userManager.FindByEmailAsync(model.Email);
             if (user == null || !user.EmailConfirmed)
             {
                 // Don't reveal that the user does not exist or is not confirmed
                 return RedirectToAction(nameof(ForgotPasswordConfirmation));
             }
 
-            var code = await UserManager.GeneratePasswordResetTokenAsync(user);
+            var code = await _userManager.GeneratePasswordResetTokenAsync(user);
             var callbackUrl = Url.Action(
                 action: "ResetPassword",
                 controller: "Sign",
                 values: new { userId = $"{user.Id}", code, area = "Account" },
                 protocol: Request.Scheme);
 
-            await EmailSender.SendEmailAsync(
+            await _emailSender.SendEmailAsync(
                 email: model.Email,
-                subject: "Reset Password",
-                message: $"Please reset your password by clicking here: <a href='{callbackUrl}'>link</a>");
+                subject: _localizer["Reset Password"],
+                message: _localizer["Please reset your password by clicking here: <a href='{0}'>link</a>", callbackUrl]);
 
             await HttpContext.AuditAsync(
                 "sent forgot password email",
@@ -423,11 +431,11 @@ namespace SatelliteSite.IdentityModule.Controllers
         public async Task<IActionResult> ResetPassword(ResetPasswordModel model)
         {
             if (!ModelState.IsValid) return View(model);
-            var user = await UserManager.FindByEmailAsync(model.Email);
+            var user = await _userManager.FindByEmailAsync(model.Email);
 
             // Don't reveal that the user does not exist
             if (user == null) return RedirectToAction(nameof(ResetPasswordConfirmation));
-            var result = await UserManager.ResetPasswordAsync(user, model.Code, model.Password);
+            var result = await _userManager.ResetPasswordAsync(user, model.Code, model.Password);
 
             if (!result.Succeeded) return ErrorView(result, model);
 
@@ -454,10 +462,10 @@ namespace SatelliteSite.IdentityModule.Controllers
         {
             if (code == null) return View("ConfirmEmailError");
 
-            var user = await UserManager.FindByIdAsync(userId);
+            var user = await _userManager.FindByIdAsync(userId);
             if (user == null) return View("ConfirmEmailError");
 
-            var result = await UserManager.ConfirmEmailAsync(user, code);
+            var result = await _userManager.ConfirmEmailAsync(user, code);
             return View(result.Succeeded ? "ConfirmEmail" : "ConfirmEmailError");
         }
 
@@ -477,23 +485,25 @@ namespace SatelliteSite.IdentityModule.Controllers
 
         private ShowMessageResult TwoFactorFail()
             => Message(
-                title: "Two-factor authentication",
-                message: "Unable to load two-factor authentication user.",
+                title: _localizer["Two-factor authentication"],
+                message: _localizer["Unable to load two-factor authentication user."],
                 type: BootstrapColor.danger);
 
         private ShowMessageResult ExternalRegisterClosed()
             => Message(
-                title: "Registration closed",
-                message: "The external login doesn't associate any accounts.\n" +
-                    "And the registration of current site is closed now.\n" +
-                    "If you believe this was a mistake, please contact the site administrator.",
+                title: _localizer["Registration closed"],
+                message: _localizer[
+                    "The external login doesn't associate any accounts. " +
+                    "And the registration of current site is closed now. " +
+                    "If you believe this was a mistake, please contact the site administrator."],
                 type: BootstrapColor.secondary);
 
         private ShowMessageResult RegisterClosed()
             => Message(
-                title: "Registration closed",
-                message: "The registration of current site is closed now.\n" +
-                    "If you believe this was a mistake, please contact the site administrator.",
+                title: _localizer["Registration closed"],
+                message: _localizer[
+                    "The registration of current site is closed now. " +
+                    "If you believe this was a mistake, please contact the site administrator."],
                 type: BootstrapColor.secondary);
     }
 }
